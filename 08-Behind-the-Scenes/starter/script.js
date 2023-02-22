de0e2f690480b62a7d14ed03516a7427f31febd8
Fernandes Ferreira <ferolder@gmail.com>
2023-02-22 12:32:59 -0300

    Scope chain

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2023 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `You're a millenial, ${firstName}!`;
+      console.log(str);
+    }
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Fernandes';
+calcAge(1993);
+
+// console.log(age); // Not accessible
+// printAge(); // Not accessible
